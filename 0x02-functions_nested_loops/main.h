bb49f6add43a05f07eb8aa809f15f58febbf7582
Azhary <mahmoudalazhary2@gmail.com>
2023-06-20 23:53:54 +0300

    Added header guard

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-#include <unistd.h>
+#ifndef HEADER_GUARD
+#define HEADER_GUARD
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+int _putchar(char);
+
+#endif
