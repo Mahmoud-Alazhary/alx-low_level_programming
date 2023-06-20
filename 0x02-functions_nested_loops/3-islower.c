dadb7dd2ac2527e87ca9b851929ca5357a6b0ddf
Azhary <mahmoudalazhary2@gmail.com>
2023-06-21 00:35:36 +0300

    Modified 3-islower.c to be in betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _islower - checks if c is a lowercase character
+ * @c: The character to be checked.
+ * Return: 1 if it is lowercase, 0 if otherwise
  */
 
 int _islower(int c)
