ea5b76199ee85bc0a4c316401dc8f6e96b3ca28a
Azhary <mahmoudalazhary2@gmail.com>
2023-06-20 22:50:22 +0300

    First Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Printing "_putchar" using a function called "_putchar"
+ *
+ * Return: 0 If successful
+ *
+ */
+int main()
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a project in the ALX SWE Program.
+The project is called: 0x02. C - Functions, nested loops
